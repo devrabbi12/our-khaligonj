5d73ff933f89a91a1e63d0c1a68f69eaad12bddc
devrabbi12 <nissharthofacebooker@gmail.com>
2025-01-18 22:57:46 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+
+var a = 10;
+var a = 20;
+a = 'Bangladesh';
+{
+  var blocked = '30';
+  console.log(a);
+}
+console.log(blocked);
+
+console.log(a);
+
+let firstName;
+let FirstName;
+
+let fruits = 'Orange';
+var fastName = Mahedi;
+var lastName=Hash;
+var country = 'fastName+lastName';
+console.log(country);
